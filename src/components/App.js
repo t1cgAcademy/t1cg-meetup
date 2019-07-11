d30919d6e81df8286b2806672b806d359f160bda
Ashell Fernando <ashellz2010@gmail.com>
2019-07-11 11:10:05 -0400

    converted to class

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
+//react imports
 import React from 'react';
-import logo from '../assets/images/logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//css
 import '../assets/css/App.css';
+import logo from '../assets/images/logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+//components
+import notFound from '../pages/404';
+import Landing from '../pages/Landing';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={notFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
