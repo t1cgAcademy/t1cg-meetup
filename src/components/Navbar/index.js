e8f338fbff709c0888ab67e32da19b658c548285
Ashell Fernando <ashellz2010@gmail.com>
2019-07-12 13:43:17 -0400

    updated navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+/* 
+I totally forgot the link's we're supposed to have but made some made-up pages
+*/
+
 export default function Navbar(props) {
   return (
     <div>
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-2">
         <Link to={'/'} className="navbar-brand">
-          {props.name}
+          {props.siteName}
         </Link>
         <button
           className="navbar-toggler"
@@ -22,24 +26,37 @@ export default function Navbar(props) {
 
         <div className="collapse navbar-collapse" id="navbarsExample03">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <Link to={'/'} className="nav-link">
+            <li className="nav-item">
+              <Link
+                to={'/'}
+                onClick={() => props.handlePageChange('Home')}
+                className={
+                  props.currentPage === 'Home' ? 'nav-link active' : 'nav-link'
+                }
+              >
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={'/link'} className="nav-link">
-                Test Link
+              <Link
+                onClick={() => props.handlePageChange('About')}
+                className={
+                  props.currentPage === 'About' ? 'nav-link active' : 'nav-link'
+                }
+              >
+                About
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={'/link'} className="nav-link">
-                Test Link
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link to={'/link'} className="nav-link">
-                Test Link
+              <Link
+                onClick={() => props.handlePageChange('Contact')}
+                className={
+                  props.currentPage === 'Contact'
+                    ? 'nav-link active'
+                    : 'nav-link'
+                }
+              >
+                Contact
               </Link>
             </li>
           </ul>
