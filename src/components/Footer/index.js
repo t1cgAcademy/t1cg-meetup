fdb5ba00a62d5bcd961280a1b464ef71b0f756f7
Ashell Fernando <ashellz2010@gmail.com>
2019-07-17 11:26:13 -0400

    footer template..

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,12 +3,35 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="fixed-bottom">
-      <p>This site is created for the Meetup with ♥</p>
-      <p>
-        <a href="https://github.com/t1cgAcademy/t1cg-meetup" target="_blank">
-          T1CG Meetup Repo
-        </a>
-      </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <p>This site is created for the Meetup with ♥</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6">
+            <p className="meetUpRepo">
+              <a
+                href="https://github.com/t1cgAcademy/t1cg-meetup"
+                target="_blank"
+              >
+                T1CG Meetup Repo
+              </a>
+            </p>
+          </div>
+          <div className="col-sm-6">
+            <p className="meetUpLink">
+              <a
+                href="https://www.meetup.com/T1CG-Academy/events/"
+                target="_blank"
+              >
+                Check us out on Meetup
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
