72e40b77fddabfcc68f82e118ed54eb44e3f6b25
Ashell Fernando <ashellz2010@gmail.com>
2019-07-17 12:14:40 -0400

    changed classnames to components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,37 +1,40 @@
 import React from 'react';
 
+//componenets
+import Container from '../Container';
+import Row from '../Row';
+import Col from '../Col';
+
 export default function Footer() {
   return (
     <footer className="fixed-bottom">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
+      <Container fluid={true}>
+        <Row fluid={true}>
+          <Col size={'sm-12'}>
             <p>This site is created for the Meetup with ♥</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-6">
-            <p className="meetUpRepo">
-              <a
-                href="https://github.com/t1cgAcademy/t1cg-meetup"
-                target="_blank"
-              >
-                T1CG Meetup Repo
-              </a>
-            </p>
-          </div>
-          <div className="col-sm-6">
-            <p className="meetUpLink">
-              <a
-                href="https://www.meetup.com/T1CG-Academy/events/"
-                target="_blank"
-              >
-                Check us out on Meetup
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+        <Row fluid={false} justifyContent={'center'}>
+          <Col size={'sm-6'}>
+            <a
+              href="https://github.com/t1cgAcademy/t1cg-meetup"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              T1CG Meetup Repo
+            </a>
+          </Col>
+          <Col size={'sm-6'}>
+            <a
+              href="https://www.meetup.com/T1CG-Academy/events/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Check us out on Meetup
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
