1ff8c7b16b080f8537f7df5333c0936e05307b7e
Ashell Fernando <ashellz2010@gmail.com>
2019-07-24 17:17:03 -0400

    updated card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './style.css';
 
-function Card(props) {
+function Card({ heading, children }) {
   return (
     <div className="card text-center">
       <div className="card-header">
-        <h2>{props.heading}</h2>
+        <h2>{heading}</h2>
       </div>
-      <div className="card-body">{props.children}</div>
+      <div className="card-body content">{children}</div>
     </div>
   );
 }
