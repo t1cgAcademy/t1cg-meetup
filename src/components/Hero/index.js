1c91d6dead517354d105a6dfd3b210ab3a37321b
Ashell Fernando <ashellz2010@gmail.com>
2019-07-24 13:48:06 -0400

    destructured Hero component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
-function Hero(props) {
+function Hero({ backgroundImage, children }) {
   return (
-    <div className="hero text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
-      {props.children}
+    <div
+      className="hero text-center"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="pt-5 text-info">{children} </div>
     </div>
   );
 }
