a7773777b99bed06cce8f8ac3195aae50467e4ee
Ashell Fernando <ashellz2010@gmail.com>
2019-07-24 17:17:46 -0400

    added rows/cols/cards to test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Jumbotron from '../../components/Jumbotron';
 import Hero from '../../components/Hero';
 import { Col, Row, Container } from '../../components/Grid';
+import Card from '../../components/Card';
 
 const Home = () => (
   <Jumbotron fluid={true}>
@@ -16,8 +17,58 @@ const Home = () => (
     </Hero>
 
     <Container fluid={true}>
-      <Row fluid={true} justifyContent={'center'}>
-        <Col size={'md-12'}>Test</Col>
+      <Row fluid={false} justifyContent={'center'}>
+        <Col size={'md-6'}>
+          <Card heading={'This has to be a component..'}>
+            <p className="lead border rounded py-3 px-4 mt-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laudantium est quasi rerum perferendis explicabo voluptatibus
+              totam impedit quia. Cumque tenetur ab atque odio dicta vitae.
+              <h3>General Schedule will go here</h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum et
+              quisquam beatae perferendis deserunt corporis facere, nemo
+              accusantium vero ipsum, cum animi quasi nulla nostrum?
+            </p>
+          </Card>
+        </Col>
+        <Col size={'md-6'}>
+          <p className="lead border rounded py-3 px-4 mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            est quasi rerum perferendis explicabo voluptatibus totam impedit
+            quia. Cumque tenetur ab atque odio dicta vitae.
+            <h1>This has to be a component too..</h1>
+            <h3>Past/Future Events will go here...</h3>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod
+            voluptas consequuntur in blanditiis optio quisquam aliquam nisi sed
+            vel dolorum! Possimus dicta in harum molestias?
+          </p>
+        </Col>
+      </Row>
+      <Row fluid={false} justifyContent={'center'}>
+        <Col size={'md-6'}>
+          <p className="lead border rounded py-3 px-4 mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            est quasi rerum perferendis explicabo voluptatibus totam impedit
+            quia. Cumque tenetur ab atque odio dicta vitae.
+            <h1>This has to be a component..</h1>
+            <h3>General Schedule will go here</h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum et
+            quisquam beatae perferendis deserunt corporis facere, nemo
+            accusantium vero ipsum, cum animi quasi nulla nostrum?
+          </p>
+        </Col>
+        <Col size={'md-6'}>
+          <p className="lead border rounded py-3 px-4 mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            est quasi rerum perferendis explicabo voluptatibus totam impedit
+            quia. Cumque tenetur ab atque odio dicta vitae.
+            <h1>This has to be a component too..</h1>
+            <h3>Past/Future Events will go here...</h3>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod
+            voluptas consequuntur in blanditiis optio quisquam aliquam nisi sed
+            vel dolorum! Possimus dicta in harum molestias?
+          </p>
+        </Col>
       </Row>
     </Container>
   </Jumbotron>
