564e702dcee1d0fd5937a17bc73ab4938584d1ee
Ashell Fernando <ashellz2010@gmail.com>
2019-07-28 14:28:39 -0400

    changed to class

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 //components
 import Jumbotron from '../../components/Jumbotron';
@@ -6,72 +6,74 @@ import Hero from '../../components/Hero';
 import { Col, Row, Container } from '../../components/Grid';
 import Card from '../../components/Card';
 
-const Home = () => (
-  <Jumbotron fluid={true}>
-    <Hero
-      backgroundImage={
-        'https://images.pexels.com/photos/1068523/pexels-photo-1068523.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-      }
-    >
-      <h1>Welcome to the T1CG Meetup Site!</h1>
-    </Hero>
+export default class Home extends Component {
+  render() {
+    return (
+      <Jumbotron fluid={true}>
+        <Hero
+          backgroundImage={
+            'https://images.pexels.com/photos/1068523/pexels-photo-1068523.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+          }
+        >
+          <h1>Welcome to the T1CG Meetup Site!</h1>
+        </Hero>
 
-    <Container fluid={true}>
-      <Row fluid={false} justifyContent={'center'}>
-        <Col size={'sm-6'}>
-          <Card heading={'General Schedule'}>
-            <div className="lead border rounded py-3 px-4 mt-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium est quasi rerum perferendis explicabo voluptatibus
-              totam impedit quia. Cumque tenetur ab atque odio dicta vitae.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum et
-              quisquam beatae perferendis deserunt corporis facere, nemo
-              accusantium vero ipsum, cum animi quasi nulla nostrum?
-            </div>
-          </Card>
-        </Col>
+        <Container fluid={true}>
+          <Row fluid={false} justifyContent={'center'}>
+            <Col size={'sm-6'}>
+              <Card heading={'General Schedule'}>
+                <div className="lead border rounded py-3 px-4 mt-3">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laudantium est quasi rerum perferendis explicabo voluptatibus
+                  totam impedit quia. Cumque tenetur ab atque odio dicta vitae.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                  et quisquam beatae perferendis deserunt corporis facere, nemo
+                  accusantium vero ipsum, cum animi quasi nulla nostrum?
+                </div>
+              </Card>
+            </Col>
 
-        <Col size={'sm-6'}>
-          <Card heading={'Past/Future Events'}>
-            <div className="lead border rounded py-3 px-4 mt-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium est quasi rerum perferendis explicabo voluptatibus
-              totam impedit quia. Cumque tenetur ab atque odio dicta vitae.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum et
-              quisquam beatae perferendis deserunt corporis facere, nemo
-              accusantium vero ipsum, cum animi quasi nulla nostrum?
-            </div>
-          </Card>
-        </Col>
-      </Row>
-      <Row fluid={false} justifyContent={'center'}>
-        <Col size={'sm-6'}>
-          <Card heading={'Member Attendance'}>
-            <div className="lead border rounded py-3 px-4 mt-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium est quasi rerum perferendis explicabo voluptatibus
-              totam impedit quia. Cumque tenetur ab atque odio dicta vitae.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum et
-              quisquam beatae perferendis deserunt corporis facere, nemo
-              accusantium vero ipsum, cum animi quasi nulla nostrum?
-            </div>
-          </Card>
-        </Col>
-        <Col size={'sm-6'}>
-          <Card heading={'Meetup/event Info/etc..'}>
-            <div className="lead border rounded py-3 px-4 mt-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium est quasi rerum perferendis explicabo voluptatibus
-              totam impedit quia. Cumque tenetur ab atque odio dicta vitae.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum et
-              quisquam beatae perferendis deserunt corporis facere, nemo
-              accusantium vero ipsum, cum animi quasi nulla nostrum?
-            </div>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  </Jumbotron>
-);
-
-export default Home;
+            <Col size={'sm-6'}>
+              <Card heading={'Past/Future Events'}>
+                <div className="lead border rounded py-3 px-4 mt-3">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laudantium est quasi rerum perferendis explicabo voluptatibus
+                  totam impedit quia. Cumque tenetur ab atque odio dicta vitae.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                  et quisquam beatae perferendis deserunt corporis facere, nemo
+                  accusantium vero ipsum, cum animi quasi nulla nostrum?
+                </div>
+              </Card>
+            </Col>
+          </Row>
+          <Row fluid={false} justifyContent={'center'}>
+            <Col size={'sm-6'}>
+              <Card heading={'Member Attendance'}>
+                <div className="lead border rounded py-3 px-4 mt-3">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laudantium est quasi rerum perferendis explicabo voluptatibus
+                  totam impedit quia. Cumque tenetur ab atque odio dicta vitae.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                  et quisquam beatae perferendis deserunt corporis facere, nemo
+                  accusantium vero ipsum, cum animi quasi nulla nostrum?
+                </div>
+              </Card>
+            </Col>
+            <Col size={'sm-6'}>
+              <Card heading={'Meetup/event Info/etc..'}>
+                <div className="lead border rounded py-3 px-4 mt-3">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laudantium est quasi rerum perferendis explicabo voluptatibus
+                  totam impedit quia. Cumque tenetur ab atque odio dicta vitae.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                  et quisquam beatae perferendis deserunt corporis facere, nemo
+                  accusantium vero ipsum, cum animi quasi nulla nostrum?
+                </div>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Jumbotron>
+    );
+  }
+}
