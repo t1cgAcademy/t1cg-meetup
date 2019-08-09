ea134e073d308ee550b53a09576272f410a5c42b
Ashell Fernando <ashellz2010@gmail.com>
2019-08-08 22:01:50 -0400

    added security and fixed padding on homepage

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 
+//security: https://www.npmjs.com/package/helmet - need to be the first of middleware stack
+app.use(
+  helmet({
+    noCache: true,
+    permittedCrossDomainPolicies: true
+  })
+);
+
 //if the host environment is in production, use files from client/build
 if (app.get('env') === 'production') {
-  app.use(express.static('client/build'));
 } else {
   //otherwise, use middleware morgan in development
-  const logger = require('morgan');
-  app.use(logger('dev'));
+}
+
+switch (app.get('env')) {
+  case 'production':
+    app.use(express.static('client/build'));
+    break;
+
+  case 'development':
+    app.use(logger('dev'));
+    break;
+
+  default:
+    app.use(express.static('client/build'));
+    break;
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
