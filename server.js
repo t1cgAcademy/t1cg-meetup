176cce95fed30b6c997cfae1833281e889fc9e33
Ashell Fernando <ashellz2010@gmail.com>
2019-08-08 21:46:13 -0400

    updated pkgs and added routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 //if the host environment is in production, use files from client/build
