ac51a2f4a762c3a54aaf295ab916cac4a40b32ac
Ashell Fernando <ashellz2010@gmail.com>
2019-09-11 11:56:28 -0400

    added new Not Found page design

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../../components/Button';
+import Jumbotron from '../../components/Jumbotron';
+
+import './style.css';
 
 export default function notFound() {
-  return (
-    <div className="jumbotron container mt-5">
-      <p className="lead text-center ">
-        404 Page Not Found
-        <div>
-          <span className="link ">
-            <Link to={'/'}>Go to Homepage</Link>
-          </span>
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-12">
+                    <Jumbotron
+                        id="notFound"
+                        fluid={true}
+                        title="Page Not Found"
+                        classNames="mb-3 text-light bg-dark"
+                        titleClassName="pt-5"
+                    >
+                        <Button btnStyle="warning" className="link mt-3">
+                            <Link id="backToHomeLink" to={'/'}>
+                                Go to Homepage
+                            </Link>
+                        </Button>
+                    </Jumbotron>
+                </div>
+            </div>
         </div>
-      </p>
-    </div>
-  );
+    );
 }
