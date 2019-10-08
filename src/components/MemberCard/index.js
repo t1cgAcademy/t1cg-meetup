dd094f941a4e9832955afa2cd35d6dd35094eb82
jonamparo <jonathanmamparo@gmail.com>
2019-10-08 13:37:43 -0400

    refactored members cards, removed long links, added personal website link location and added padding to the members page

--- src/components/MemberCard/index.js
+++ src/components/MemberCard/index.js
@@ -1,118 +1,134 @@
-import React from 'react';
-import { Container, Row, Col } from '../Grid';
+import React from "react";
+import { Container, Row, Col } from "../Grid";
 
-import './style.css';
+import "./style.css";
 
 export default function MemberCard({
-    id,
-    Name,
-    imgURL,
-    Occupation,
-    Bio,
-    Twitter,
-    Facebook,
-    LinkedIn,
-    Github
+  id,
+  Name,
+  imgURL,
+  Occupation,
+  Bio,
+  socialMedia,
+  Twitter,
+  Facebook,
+  LinkedIn,
+  Github
 }) {
-    return (
-        <div className="card memberCard">
-            <Container classNames="bg-dark">
-                <Row classNames="my-1">
-                    <Col
-                        size={'lg-12'}
-                        classNames="text-center h2 text-light memberCard-header"
-                    >
-                        {Name}
-                    </Col>
-                </Row>
-                <Row>
-                    <Col size="lg-12" classNames="text-center">
-                        <div className="img-container">
-                            <img
-                                style={{ maxWidth: '15rem' }}
-                                alt={Name}
-                                className="rounded-circle border border-dark"
-                                src={
-                                    imgURL.length < 2
-                                        ? 'https://via.placeholder.com/350x150'
-                                        : imgURL
-                                }
-                            />
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-            <div className="memberCardContent">
-                <ul>
-                    <li>
-                        <strong>Occupation:</strong>{' '}
-                        {Occupation.length < 1 ? 'Meetup Member' : Occupation}
-                    </li>
-                    <li>
-                        <strong>Bio:</strong>{' '}
-                        {Bio.length < 1 ? `Hello! My name is ${Name}` : Bio}
-                    </li>
-                    {/* the <> is a fragment, allows you to stack elements. Difference between shorthand <> vs. <React.Fragment> is, you can't pass props with shorthand */}
-                    <>
-                        {Twitter ? (
-                            <li>
-                                <strong>Twitter:</strong>{' '}
-                                <a
-                                    href={'https://www.twitter.com/' + Twitter}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    {Twitter}
-                                </a>
-                            </li>
-                        ) : (
-                            ''
-                        )}
-                        {Facebook ? (
-                            <li>
-                                <strong>Facebook:</strong>{' '}
-                                <a
-                                    href={Facebook}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    {Facebook}
-                                </a>
-                            </li>
-                        ) : (
-                            ''
-                        )}
-                        {LinkedIn ? (
-                            <li>
-                                <strong>LinkedIn:</strong>{' '}
-                                <a
-                                    href={LinkedIn}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    {LinkedIn}
-                                </a>
-                            </li>
-                        ) : (
-                            ''
-                        )}
-                        {Github ? (
-                            <li>
-                                <strong>Github:</strong>{' '}
-                                <a
-                                    href={Github}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    {Github}
-                                </a>
-                            </li>
-                        ) : (
-                            ''
-                        )}
-                    </>
-                </ul>
+  return (
+    <div className="card memberCard">
+      <Container classNames="bg-dark">
+        <Row classNames="my-1">
+          <Col
+            size={"lg-12"}
+            classNames="text-center h2 text-light memberCard-header"
+          >
+            {Name}
+          </Col>
+        </Row>
+        <Row>
+          <Col size="lg-12" classNames="text-center">
+            <div className="img-container">
+              <img
+                style={{ maxWidth: "15rem" }}
+                alt={Name}
+                className="rounded-circle border border-dark"
+                src={
+                  imgURL.length < 2
+                    ? "https://via.placeholder.com/350x150"
+                    : imgURL
+                }
+              />
             </div>
-        </div>
-    );
+          </Col>
+        </Row>
+      </Container>
+      <div className="memberCardContent">
+        <ul className="list-unstyled ml-3 mt-2">
+          <li>
+            <strong>Occupation:</strong>{" "}
+            {Occupation.length < 1 ? "Meetup Member" : Occupation}
+          </li>
+          <li>
+            <strong>Bio:</strong>{" "}
+            {Bio.length < 1 ? `Hello! My name is ${Name}` : Bio}
+          </li>
+          {/* the <> is a fragment, allows you to stack elements. Difference between shorthand <> vs. <React.Fragment> is, you can't pass props with shorthand */}
+          <>
+            {Twitter ? (
+              <li>
+                <strong>Twitter:</strong>{" "}
+                <a
+                  href={"https://www.twitter.com/" + Twitter}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {Twitter}
+                </a>
+              </li>
+            ) : (
+              ""
+            )}
+            {Facebook ? (
+              <li>
+                <strong>Facebook:</strong>{" "}
+                <a
+                  href={"https://www.facebook.com/" + Facebook}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {Facebook}
+                </a>
+              </li>
+            ) : (
+              ""
+            )}
+            {LinkedIn ? (
+              <li>
+                <strong>LinkedIn:</strong>{" "}
+                <a
+                  href={"https://www.linkedin.com/in/" + LinkedIn}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {LinkedIn}
+                </a>
+              </li>
+            ) : (
+              ""
+            )}
+            {Github ? (
+              <li>
+                <strong>Github:</strong>{" "}
+                <a
+                  href={"https://github.com/" + Github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {Github}
+                </a>
+              </li>
+            ) : (
+              ""
+            )}
+            {socialMedia ? (
+              <li>
+                <strong>Website:</strong>{" "}
+                <a
+                  href={"" + socialMedia}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Visit Site
+                  {/* {socialMedia} */}
+                </a>
+              </li>
+            ) : (
+              ""
+            )}
+          </>
+        </ul>
+      </div>
+    </div>
+  );
 }
