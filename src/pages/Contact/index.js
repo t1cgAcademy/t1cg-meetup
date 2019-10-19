b44c52816d2891a600ff45c7eacfa2d1fe1ce280
Alexander Vela <alexandervela96@yahoo.com>
2019-10-18 22:24:00 -0400

    added a simple form to the contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import './style.css';
 
+//Components
+import Wrapper from '../../components/Wrapper';
+import { Container, Row, Col } from '../../components/Grid';
+
 const Contact = () => (
-  <div>
-    <h1 className="contact-header">Contact Page</h1>
+  <Wrapper>
+    <Container fluid={true}>
+    <Row classNames="mt-4">
+    <Col size={'md-6'}>
+    <h1 className="contact-header">Get In Touch With Us <i class="far fa-comment-alt"></i></h1>
     <p className="contact-text">
-      Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-      molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-      magna a ultrices. Aenean pellentesque placerat lacus imperdiet efficitur.
-      In felis nisl, luctus non ante euismod, tincidunt bibendum mi. In a
-      molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna posuere,
-      eget tristique dui dapibus. Maecenas fermentum elementum faucibus. Quisque
-      nec metus vestibulum, egestas massa eu, sollicitudin ipsum. Nulla
-      facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh dictum
-      ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia
-      nostra, per inceptos himenaeos. Etiam ornare rutrum felis at rhoncus.
-      Etiam vel condimentum magna, quis tempor nulla.
+    Integer cursus bibendum sem non pretium. 
+    Vestibulum in aliquet sem, quis molestie urna. 
+    Aliquam semper ultrices varius. 
+    Aliquam faucibus sit amet magna a ultrices. 
+    Aenean pellentesque placerat lacus imperdiet efficitur. 
+    In felis nisl, luctus non ante euismod, tincidunt bibendum mi. 
+    In a molestie nisl, eu sodales diam.
     </p>
-  </div>
+    </Col>
+    <Col size={'md-6'}>
+      <form action="" className="contact-form" method="">
+        <div className="form-group">
+          <label for="email">Email address</label>
+          <input type="email" className="form-control border-primary" id="email" aria-describedby="emailHelp" placeholder="Please enter your email"></input>
+        </div>
+        <div className="form-group">
+          <label for="name">Full Name</label>
+          <input type="text" className="form-control border-primary" id="name" aria-describedby="nameHelp" placeholder="Please enter your full name"></input>
+        </div>
+        <div className="form-group">
+          <label for="textarea">Message</label>
+          <textarea className="form-control border-primary" id="textarea" name="textarea" maxlength="256" rows="4" placeholder="Please enter your message"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary submit-button">Send</button>
+      </form>
+    </Col>
+    </Row>
+    </Container>
+  </Wrapper>
 );
 
 export default Contact;
